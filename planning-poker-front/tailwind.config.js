86a39cfd1dd3a517d1077bdf2d9715cc1800ceb0
Gummiees <xavier.caro@koahealth.com>
2022-03-25 13:22:36 +0100

    Initial commit

---
+++ planning-poker-front/tailwind.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      lineHeight: {
+        sm: '0.875rem'
+      },
+      fontSize: {
+        '3xl': '2rem'
+      },
+      backgroundOpacity: {
+        85: '0.85'
+      },
+      colors: {
+        indigo: {
+          material: '#3F51B5'
+        },
+        deepBlue: {
+          DEFAULT: '#020033'
+        }
+      },
+      spacing: {
+        auto: 'auto',
+        '1px': '1px',
+        '2px': '2px',
+        '3px': '3px',
+        'mat-select': '1.34375em'
+      },
+      transitionProperty: {
+        padding: 'padding',
+        margin: 'margin',
+        'bg-color': 'background-color'
+      },
+      transitionDuration: {
+        400: '400ms'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+  important: true
+};
